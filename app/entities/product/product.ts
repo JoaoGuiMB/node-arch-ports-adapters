ee1f7b9dc8a6c6a2fc8b72c2fea5fe0c73b644e2
JoaoGuiMB <juaogui159@gmail.com>
2024-04-13 11:09:48 -0300

    feat: implelementing Product entity

---
+++ app/entities/product/product.ts
@@ -0,0 +1,49 @@
+import { v4 as uuid } from "uuid";
+import { ProductInterface, ProductStatus } from "./types/product";
+
+export default class Product implements ProductInterface {
+  private id: string;
+  private name: string;
+  private price: number;
+  private status: ProductStatus = ProductStatus.DISABLE;
+
+  constructor(name: string, price: number) {
+    this.id = uuid();
+    this.name = name;
+    this.price = price;
+  }
+
+  enable(): void {
+    if (this.price > 0) {
+      this.status = ProductStatus.ENABLE;
+      return;
+    }
+    throw new Error(
+      "the price must be greater than zero to enable the product"
+    );
+  }
+
+  disable(): void {
+    if (this.price === 0) {
+      this.status = ProductStatus.DISABLE;
+      return;
+    }
+    throw new Error("the price must be zero to disable the product");
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getPrice(): number {
+    return this.price;
+  }
+
+  getStatus(): ProductStatus {
+    return this.status;
+  }
+}
