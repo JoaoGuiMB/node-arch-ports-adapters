14b14110ddba16368cfe1ea60c72ea0db2633e1b
JoaoGuiMB <juaogui159@gmail.com>
2024-04-14 16:37:54 -0300

    tests: save product

--- adapters/db/product.test.ts
+++ adapters/db/product.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeAll, afterAll } from "vitest";
 import { Database } from "sqlite3";
 import ProductAdapter from "./product";
+import Product from "../../app/product/entities/product";
 
 describe("ProductDb unit tests", () => {
   let db = {} as Database;
@@ -25,5 +26,21 @@ describe("ProductDb unit tests", () => {
     const productAdapter = new ProductAdapter(db);
     const product = await productAdapter.get("2");
     expect(product).toBeUndefined();
+  });
+
+  it("should create a product", async () => {
+    const product = new Product("product", 10);
+    const productAdapter = new ProductAdapter(db);
+    const savedProduct = await productAdapter.save(product);
+    expect(savedProduct).toBe(product);
+  });
+
+  it("should update a product", async () => {
+    const product = new Product("product", 10);
+    const productAdapter = new ProductAdapter(db);
+    await productAdapter.save(product);
+    product.setPrice(20);
+    const savedProduct = await productAdapter.save(product);
+    expect(savedProduct).toBe(product);
   });
 });
