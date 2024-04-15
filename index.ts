784fb68c33ad4835f6ef8b972f77a5bec0304611
JoaoGuiMB <juaogui159@gmail.com>
2024-04-15 20:19:55 -0300

    feat: implementing web server

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import server from "./adapters/web/server/server";
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT).on("listening", () => {
+  console.log(`We are live on http://localhost:${PORT}`);
+});
